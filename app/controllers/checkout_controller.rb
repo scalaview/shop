56b79f0db87ce22ffe387243b6bdcee80bb20fbc
benko <chailink100@gmail.com>
2015-12-07 17:15:10 +0800

    add checkout show

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,7 @@
 class CheckoutController < ApplicationController
 
+  layout "checkout"
+
   before_filter :login_required
   # before_filter :ready, :only => [:show]
 
@@ -12,22 +14,16 @@ class CheckoutController < ApplicationController
 
   def ready
     @customer = current_customer
-    if params[:address_id]
-      @address = @customer.addresses.find params[:address_id]
-    else
-      # if has the default address use it or redirect to new
-      @address = @customer.addresses.default
-    end
-
-    unless @address.present?
-      redirect_to new_address_path and return
-    end
-
     @order = current_customer.current_order
-
     @delivery_address = @order.delivery_address
 
-    unless @address.match?(@delivery_address)
+    unless @delivery_address.present?
+      # if has the default address use it or redirect to new
+      @address = @customer.addresses.default
+      unless @address.present?
+        redirect_to new_address_path and return
+      end
+
       unless @address.generate_order_address(order, Shoppe::Address.TYPES[:delivery])
         redirect_to shopping_cart_path
       end
