3e7b93a6323b8ce604dc276762b29059be7724d2
scalaview <chailink100@gmail.com>
2015-12-11 16:48:25 +0800

    use sui

--- app/assets/javascripts/shopping_cart.js
+++ app/assets/javascripts/shopping_cart.js
@@ -1,4 +1,2 @@
-//= require jquery/dist/jquery.min
+//= require zepto/zepto.min
 //= require angular/angular.min
-//= require angular-bootstrap/ui-bootstrap.min
-//= require angular-bootstrap/ui-bootstrap-tpls.min
