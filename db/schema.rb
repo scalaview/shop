919c4006398fbad1ddccd044455b3c75f8a826fe
benko <chailink100@gmail.com>
2015-12-16 22:28:13 +0800

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208093733) do
+ActiveRecord::Schema.define(version: 20151216090107) do
 
   create_table "nifty_key_value_store", force: :cascade do |t|
     t.integer "parent_id",   limit: 4
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20151208093733) do
     t.string   "city",          limit: 255
     t.string   "area",          limit: 255
     t.string   "street",        limit: 255
-    t.string   "location",      limit: 255
     t.string   "md5",           limit: 255
+    t.string   "location",      limit: 255
     t.string   "phone",         limit: 255
     t.string   "receiver_name", limit: 255
   end
@@ -307,29 +307,46 @@ ActiveRecord::Schema.define(version: 20151208093733) do
   add_index "shoppe_product_translations", ["locale"], name: "index_shoppe_product_translations_on_locale", using: :btree
   add_index "shoppe_product_translations", ["shoppe_product_id"], name: "index_shoppe_product_translations_on_shoppe_product_id", using: :btree
 
-  create_table "shoppe_products", force: :cascade do |t|
-    t.integer  "parent_id",         limit: 4
-    t.string   "name",              limit: 255
-    t.string   "sku",               limit: 255
-    t.string   "permalink",         limit: 255
-    t.text     "description",       limit: 65535
-    t.text     "short_description", limit: 65535
-    t.boolean  "active",                                                  default: true
-    t.decimal  "weight",                          precision: 8, scale: 3, default: 0.0
-    t.decimal  "price",                           precision: 8, scale: 2, default: 0.0
-    t.decimal  "cost_price",                      precision: 8, scale: 2, default: 0.0
-    t.integer  "tax_rate_id",       limit: 4
+  create_table "shoppe_product_variant_values", force: :cascade do |t|
+    t.integer  "stockkeeping_unit_id", limit: 4, null: false
+    t.integer  "variant_value_id",     limit: 4, null: false
+    t.integer  "variant_type_id",      limit: 4, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "featured",                                                default: false
-    t.text     "in_the_box",        limit: 65535
-    t.boolean  "stock_control",                                           default: true
-    t.boolean  "default",                                                 default: false
   end
 
-  add_index "shoppe_products", ["parent_id"], name: "index_shoppe_products_on_parent_id", using: :btree
+  add_index "shoppe_product_variant_values", ["stockkeeping_unit_id"], name: "index_shoppe_product_variant_values_on_stockkeeping_unit_id", using: :btree
+  add_index "shoppe_product_variant_values", ["variant_type_id"], name: "index_shoppe_product_variant_values_on_variant_type_id", using: :btree
+  add_index "shoppe_product_variant_values", ["variant_value_id"], name: "index_shoppe_product_variant_values_on_variant_value_id", using: :btree
+
+  create_table "shoppe_products", force: :cascade do |t|
+    t.string   "name",                         limit: 255
+    t.string   "permalink",                    limit: 255
+    t.text     "description",                  limit: 65535
+    t.text     "short_description",            limit: 65535
+    t.boolean  "active",                                     default: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "featured",                                   default: false
+    t.text     "in_the_box",                   limit: 65535
+    t.integer  "default_stockkeeping_unit_id", limit: 4
+  end
+
+  add_index "shoppe_products", ["default_stockkeeping_unit_id"], name: "index_shoppe_products_on_default_stockkeeping_unit_id", using: :btree
   add_index "shoppe_products", ["permalink"], name: "index_shoppe_products_on_permalink", using: :btree
-  add_index "shoppe_products", ["sku"], name: "index_shoppe_products_on_sku", using: :btree
+
+  create_table "shoppe_regions", force: :cascade do |t|
+    t.string   "code",       limit: 255, null: false
+    t.string   "parentcode", limit: 255
+    t.string   "name",       limit: 255, null: false
+    t.integer  "level",      limit: 4,   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "shoppe_regions", ["code"], name: "index_shoppe_regions_on_code", using: :btree
+  add_index "shoppe_regions", ["level"], name: "index_shoppe_regions_on_level", using: :btree
+  add_index "shoppe_regions", ["parentcode"], name: "index_shoppe_regions_on_parentcode", using: :btree
 
   create_table "shoppe_settings", force: :cascade do |t|
     t.string "key",        limit: 255
@@ -353,6 +370,25 @@ ActiveRecord::Schema.define(version: 20151208093733) do
   add_index "shoppe_stock_level_adjustments", ["item_id", "item_type"], name: "index_shoppe_stock_level_adjustments_items", using: :btree
   add_index "shoppe_stock_level_adjustments", ["parent_id", "parent_type"], name: "index_shoppe_stock_level_adjustments_parent", using: :btree
 
+  create_table "shoppe_stockkeeping_units", force: :cascade do |t|
+    t.string   "product_id",    limit: 255,                                        null: false
+    t.string   "sku",           limit: 255
+    t.boolean  "active",                                            default: true
+    t.boolean  "stock_control",                                     default: true
+    t.decimal  "weight",                    precision: 8, scale: 3, default: 0.0
+    t.decimal  "cost_price",                precision: 8, scale: 3, default: 0.0
+    t.decimal  "price",                     precision: 8, scale: 3, default: 0.0
+    t.integer  "tax_rate_id",   limit: 4
+    t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "shoppe_stockkeeping_units", ["active"], name: "index_shoppe_stockkeeping_units_on_active", using: :btree
+  add_index "shoppe_stockkeeping_units", ["deleted_at"], name: "index_shoppe_stockkeeping_units_on_deleted_at", using: :btree
+  add_index "shoppe_stockkeeping_units", ["product_id"], name: "index_shoppe_stockkeeping_units_on_product_id", using: :btree
+  add_index "shoppe_stockkeeping_units", ["stock_control"], name: "index_shoppe_stockkeeping_units_on_stock_control", using: :btree
+
   create_table "shoppe_tax_rates", force: :cascade do |t|
     t.string   "name",         limit: 255
     t.decimal  "rate",                       precision: 8, scale: 2
@@ -372,5 +408,22 @@ ActiveRecord::Schema.define(version: 20151208093733) do
   end
 
   add_index "shoppe_users", ["email_address"], name: "index_shoppe_users_on_email_address", using: :btree
+
+  create_table "shoppe_variant_types", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "shoppe_variant_values", force: :cascade do |t|
+    t.integer  "variant_type_id", limit: 4
+    t.string   "name",            limit: 255
+    t.integer  "position",        limit: 4,   default: 0
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "shoppe_variant_values", ["position"], name: "index_shoppe_variant_values_on_position", using: :btree
+  add_index "shoppe_variant_values", ["variant_type_id"], name: "index_shoppe_variant_values_on_variant_type_id", using: :btree
 
 end
