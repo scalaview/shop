200140cb11ca4f65bde23df31503b95941ca4dc3
hmbin <334777557@qq.com>
2015-12-17 15:48:28 +0800

    user-info js done

--- app/assets/javascripts/checkout.js
+++ app/assets/javascripts/checkout.js
@@ -1,3 +1,4 @@
 //= require zepto/zepto.min
 //= require zepto.csrf
 //= require checkout/order
+//= require user_info.js
---
+++ app/assets/javascripts/user_info.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+  $(document).on("pageInit", "#page-address-new", function(e, pageId, $page) {
+    appendSelectAhead($("#select_city"));
+    appendSelectAhead($("#select_district"));
+  });
+  $(document).on('change', "#select_provice", function(){
+    changeProvince();
+  });
+  $(document).on('change', "#select_city", function(){
+    changeCity();
+  });
+});
+function changeProvince(){
+  /*清空city、district*/
+  appendSelectAhead($("#select_city"));
+  appendSelectAhead($("#select_district"));
+  /*获取city数据*/
+  var selectValue = $("#select_provice").val();
+  if(!selectValue){
+    return;
+  }
+  $.ajax({
+    type:"post",
+    url:"/mobile/region_city",
+    data:{"provice": selectValue},
+    dataType:"json",
+    success:function(data){
+      if(data && data.length>0){
+        for (var item in data) {
+          $("#select_city").append("<option value='"+data[item].code+"'>"+data[item].name+"</option>");
+        };
+      }
+    },
+    error:function(XMLHttpRequest, textStatus, errorThrown){
+    }
+  });
+}
+function changeCity(){
+  /*清空district*/
+  appendSelectAhead($("#select_district"));
+  /*获取district数据*/
+  var selectValue = $("#select_city").val();
+  if(!selectValue){
+    return;
+  }
+  $.ajax({
+    type:"post",
+    url:"/mobile/region_district",
+    data:{"city": selectValue},
+    dataType:"json",
+    success:function(data){
+      if(data && data.length>0){
+        for (var item in data) {
+          $("#select_district").append("<option value='"+data[item].code+"'>"+data[item].name+"</option>");
+        };
+      }
+    },
+    error:function(XMLHttpRequest, textStatus, errorThrown){
+    }
+  });
+}
+/*在最前面加个选项*/
+function appendSelectAhead(select){
+  select.empty();
+  select.prepend("<option value=''>-- 请选择 --</option>");
+}
