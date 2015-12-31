8e9bb039f1a913b047e8f0cebc3f5f06ac9429fa
scalaview <chailink100@gmail.com>
2015-12-31 14:06:15 +0800

    add product

--- app/controllers/mobile/pages_controller.rb
+++ app/controllers/mobile/pages_controller.rb
@@ -3,7 +3,7 @@ class Mobile::PagesController < ApplicationController
   layout "mobile"
 
   def index
-    # @products = Shoppe::Product.active.featured.includes(:product_categories, :variants).root
+    @products = Shoppe::Product.active.featured.includes(:product_categories, :variants).root
   end
 
 end
